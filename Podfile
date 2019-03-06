2bd48928cbf922707233186b05d26d57b0918b73
EdgarJan <krodanix@gmail.com>
2019-03-06 15:44:30 +0200

    First commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '12.1'
+use_frameworks!
+
+target 'walker' do
+  pod 'Mapbox-iOS-SDK', '~> 4.9'
+  pod 'Turf', '~> 0.3'
+  pod 'MapboxNavigation', '~> 0.29.0'
+end
---
+++ walker/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  walker
+//
+//  Created by Edgar Jan Vuicik on 05/03/2019.
+//  Copyright © 2019 Sistemiun. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import Mapbox
+import MapboxCoreNavigation
+import MapboxNavigation
+import MapboxDirections
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var mapView: MGLMapView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.showsUserLocation = true
+        mapView.showsUserHeadingIndicator = true
+        mapView.setUserTrackingMode(.followWithHeading, animated: true)
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        var coordsPointer = UnsafeMutablePointer<CLLocationCoordinate2D>.allocate(capacity: Int(polyline.pointCount))
+//        polyline.getCoordinates(coordsPointer, range: NSMakeRange(0, Int(polyline.pointCount)))
+//
+//        // save coords
+//        var lineCoords: [CLLocationCoordinate2D] = []
+//        for i in 0..<polyline.pointCount {
+//            lineCoords.append(coordsPointer[Int(i)])
+//        }
+//
+//        let lineString:LineString = LineString(geometry: lineCoords)
+//
+//        let bufferLineString = SwiftTurf.buffer(lineString, distance: width, units: .Meters)
+//
+//        let outer = bufferLineString!.geometry![0]
+//        let interiors = bufferLineString?.geometry![1..<bufferLineString!.geometry.count].map({ coords in
+//            return MGLPolygon(coordinates: coords, count: UInt(coords.count))
+//        })
+//        // This polygon is solution
+//        self.currentBufferPolygon = MGLPolygon(coordinates: outer, count: UInt(outer.count), interiorPolygons: interiors)
+//        mapView.addAnnotation(self.currentBufferPolygon!)
+    }
+
+
+}
